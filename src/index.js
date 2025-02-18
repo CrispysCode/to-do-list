da033fa2a7e77401c8f26b5e020b97f381c483e0
CrispysCode <pr0ject0dinx@gmail.com>
2025-02-18 07:07:49 -0600

    cleaned up unnecessary coments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import "./styles.css";
-import { greeting } from "./modules/startModule.js";
 import { formGenerator, loadProjects } from "./modules/formGenerator.js";
-//import anyImage from "./image.png";
-//const image = document.createElement("img");
-// image.src = anyImage;
-// document.body.appendChild(image);
 
-console.log(greeting);
 
 formGenerator();
 loadProjects();

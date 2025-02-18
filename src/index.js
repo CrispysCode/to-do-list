2a2d4cf71d0b771ffa764ec4b970f30443998611
CrispysCode <pr0ject0dinx@gmail.com>
2025-02-18 00:37:58 -0600

    initialized advanced webpack bundle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import "./styles.css";
 import { greeting } from "./startModule.js";
+//import anyImage from "./image.png";
+//const image = document.createElement("img");
+// image.src = anyImage;
+// document.body.appendChild(image);
 
 console.log(greeting);

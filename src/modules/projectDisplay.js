95c51bc0bed3d1d7e083b8cc0f44ac5b46ef2199
CrispysCode <pr0ject0dinx@gmail.com>
2025-02-18 05:42:59 -0600

    using its own module we use dom manipulation to publish each project to the right.

--- src/modules/projectDisplay.js
+++ src/modules/projectDisplay.js
@@ -1,3 +1,15 @@
 export function projectDisplay(projectData) {
-    const
-}
+    const projectBox = document.createElement("div");
+    projectBox.classList.add("project-box");
+
+    projectBox.innerHTML = 
+      `
+        <h3>${projectData.title}</h3>
+        <p>${projectData.description}</p>
+        <p>Due: ${projectData.dueDate}</p>
+        <p>Priority:${projectData.priority}</p>
+        <p>Notes: ${projectData.notes}</p>
+        <p>Checked is complete: ${projectData.status ? 'Completed' : 'Pending'}</p>
+        `;
+        return projectBox;
+    }

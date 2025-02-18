bd4cb252d863d36125f896d93278e76e1ddc75d0
CrispysCode <pr0ject0dinx@gmail.com>
2025-02-18 02:29:46 -0600

    finished some styling and added webpack dev server.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { greeting } from "./startModule.js";
+import { greeting } from "./modules/startModule.js";
 //import anyImage from "./image.png";
 //const image = document.createElement("img");
 // image.src = anyImage;
